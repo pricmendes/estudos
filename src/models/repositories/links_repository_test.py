3c3368d4033ddad591a70d7e6282d7fdfaa812f1
Priscila Mendes <priscm.n@gmail.com>
2024-07-09 17:00:41 -0300

    atualizando repositorio links

--- src/models/repositories/links_repository_test.py
+++ src/models/repositories/links_repository_test.py
@@ -5,28 +5,32 @@ from .links_repository import LinksRepository
 from src.models.settings.db_connection_handler import db_connection_handler
 
 db_connection_handler.connect()
+link_id = str(uuid.uuid4())
 trip_id = str(uuid.uuid4())
 
 @pytest.mark.skip(reason = "interação com o banco") # ignora os testes para que não sejam feitos alterações no banco de dados // caso queira testar a funcionalidade direta no banco é só retirar esse comando
 def test_registry_link():
     conn = db_connection_handler.get_connection()
-    links_repository = LinksRepository(conn)
+    link_repository = LinksRepository(conn)
 
-    links_trips_infos = {
-        "id"    : str(uuid.uuid4()),
+    link_infos = {
+        "id"    : link_id,
         "trip_id": trip_id,
         "link": "www.google.com",
         "title": "airbnb"
     }
 
-    links_repository.registry_link(links_trips_infos)
+    link_repository.registry_link(link_infos)
 
 @pytest.mark.skip(reason = "interação com o banco") # ignora os testes para que não sejam feitos alterações no banco de dados // caso queira testar a funcionalidade direta no banco é só retirar esse comando
 def test_find_link_from_trip():
     conn = db_connection_handler.get_connection()
-    links_repository = LinksRepository(conn)
+    link_repository = LinksRepository(conn)
 
-    links = links_repository.find_link_from_trip(trip_id)
+    links = link_repository.find_link_from_trip(trip_id)
+    
+    assert isinstance(links, list) # isinstance = Retorna se um objeto é uma instância de uma classe ou de uma subclasse dela.
+    assert isinstance(links[0], tuple)
     print()
     print(links)
 
