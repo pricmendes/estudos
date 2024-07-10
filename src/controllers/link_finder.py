2c68486fdd0efe70d4f5a5927efbe5c71dcf21ed
Priscila Mendes <priscm.n@gmail.com>
2024-07-10 17:21:06 -0300

    controllers

---
+++ src/controllers/link_finder.py
@@ -0,0 +1,27 @@
+from typing import Dict
+import uuid
+
+class LinkFinder:
+    def __init__(self, link_repository) -> None:
+        self.__link_repository = link_repository
+
+    def find(self, tripId):
+        try:
+            links = self.__link_repository.find_link_from_trip(tripId)
+
+            formatted_links = []
+            for link in links:
+                 formatted_links.append({
+                      "id": link[0],
+                      "url": link[2],
+                      "title": link[3],
+                })
+            return {
+                "body": { "links": formatted_links }, "status_code": 200}
+
+            
+        except Exception as exception:
+                return {
+                    "body": { "error": "Bad Request", "message": str(exception) },
+                    "status_code": 400 # o status 400 significa "Solicitação Incorreta"
+                }
