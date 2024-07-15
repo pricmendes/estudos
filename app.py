48896e92560f5cfe679dab69e8b685bfacb33a5e
Priscila Mendes <priscm.n@gmail.com>
2024-07-15 20:56:13 -0300

    ecommerce

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 # importação da biblioteca
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "Teste@123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ecommerce.db"
 
+login_manager = LoginManager(app) # gerenciamento da autenticação
 db = SQLAlchemy(app)
+login_manager.init_app(app) # recebe a aplicação
+login_manager.login_view = "login" # rota onde contém os dados para autenticar o usuário
+CORS(app) # serve para que sistemas externos possam acessar o sistema
 
 # Modelagem
+
+# User (id, username, password)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=True)
+    cart = db.relationship("CartItem", backref="user", lazy=True) # lazy recupera corrinho do usuário somente se ele tentar acessar o carrinho
+
 # Produto (id, name, price, description)
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,8 +30,37 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
 
+class CartItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey("product.id"), nullable=False)
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    # data.get("username") uma opção para recuperar o usuário
+
+    user = User.query.filter_by(username=data.get("username")).first() # "first()" para recuperar o primeiro
+    
+    if user and data.get("password") == user.password:
+            login_user(user)
+            return jsonify({"message": f"Logged in successfully - {user} "})
+
+    return jsonify({"message": "Unauthorized. Invalid credentials"}), 401
+
+@app.route("/logout", methods=["POST"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout successfully"})
+
 # cadastrar produto
+@login_manager.user_loader # autenticação
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route("/api/products/add", methods=["POST"])
+@login_required
 def add_product():
     #inserção do payload
     data = request.json
@@ -29,6 +73,7 @@ def add_product():
 
 # deletar produto
 @app.route("/api/products/delete/<int:product_id>", methods=["DELETE"])
+@login_required
 def delete_product(product_id):
     # recuperando o produto da base de dados
     # verificar se o produto existe
@@ -56,6 +101,7 @@ def get_product_details(product_id):
 
 # procurar um produto
 @app.route("/api/products/update/<int:product_id>", methods=["PUT"])
+@login_required
 def update_product(product_id):
     product = Product.query.get(product_id)
     if not product:
@@ -72,9 +118,21 @@ def update_product(product_id):
         product.description = data["description"]
 
     db.session.commit()
-    return jsonify({"message": "Product updated successfully"})     
+    return jsonify({"message": "Product updated successfully"})
 
-
+@app.route("/api/products", methods = ["GET"])     
+def get_products():
+    products = Product.query.all()
+    product_list = []
+    for product in products:
+        product_data = {
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            # "description": product.description # apagado a rota de listange descrição para que a descrição apareça somente quando o produto for selecionado 
+        }
+        product_list.append(product_data)
+    return jsonify(product_list)    
 
 # definindo a rota raiz(pagina inicial) e a função que será executada ao ser requisitada
 
