0d3b3c0ed5a81eb949787f52819e8932c3e5235a
Priscila Mendes <priscm.n@gmail.com>
2024-08-11 19:59:17 -0300

    novos

--- spamSimNao.py
+++ spamSimNao.py
@@ -47,3 +47,37 @@ previsao = model.predict(novos_emails_transformed)
 
 for i, email in enumerate(novos_emails):
     print(f'Email: "{email}" - Classificado como: {"Spam" if previsao[i] == 1 else "Não Spam"}')
+    
+
+# Modelo de Reconhecimento de Padrões para classificação de e-mails Spam Sim ou Não.
+
+# Deve-se treinar o modelo com o máximo de informações possíveis para que ele possa classificar se o email é um Spam, por isso é importante informar Modelos de Mensagens e classificá-las como Spam (1) ou Não Spam (0).
+
+# Utilizei a biblioteca Sklearn que é muito utilizada para aprendizado de máquina.
+
+# Realizado o treinamento inicial.
+
+# 1. Coleta de Pistas e carregamento do conjunto de dados.
+
+# 2. Processamento dos Dados
+
+# 3. Divisão dos Dados 
+
+# 4. Treinamento do Modelo 
+
+# 5. Fazer Previsões
+
+# 6. Acurácia do Modelo para saber o quanto o modelo acertou a previsão
+
+# 7. Testar com Novos E-mails
+
+# CountVectorizer: Transforma o texto dos e-mails em uma matriz numérica, onde cada coluna representa uma palavra e cada linha um e-mail.
+# MultinomialNB: Classifica o texto que será treinado nos dados de e-mails.
+# Acurácia: Avalia se o modelo está classificando os e-mails com base no conjunto de teste.
+# Novos E-mails: Testa o modelo com novos e-mails para ver como ele os classifica.
+
+# Retorno:
+
+# Acurácia do modelo: 1.00
+# Email: "Você ganhou um carro novo!" - Classificado como: Spam
+# Email: "Lembrete: Reunião às 10h." - Classificado como: Não Spam
