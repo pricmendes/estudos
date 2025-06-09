a1930bf22aecd8b60ebd3b968b0cef7e42dc3152
Priscila Mendes <128762353+pricmendes@users.noreply.github.com>
2025-06-08 23:09:54 -0300

    Delete elemento_lista.py

--- elemento_lista.py
+++
@@ -1,4 +0,0 @@
-class ElementoUnicoDaLista:
-    def __init__(self, elemento):
-        self.elementoDaLista = elemento # armazena o elemento atual
-        self.proximoElemento = None # armazena o próximo elemento
